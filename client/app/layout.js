a31698818d00f7956082e1853850fd1c31d4e0d0
Sawata gore <sawatagore27@gmail.com>
2024-09-13 21:49:19 +0530

    implemented toster alert

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "Blog App",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Toaster />
+        {children}
+      </body>
     </html>
   );
 }
