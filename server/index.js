b2fa9e18dc238645b342edae0a33ea208036c26b
Sawata gore <sawatagore27@gmail.com>
2024-09-13 21:19:04 +0530

    resolved CORS error

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
@@ -9,10 +10,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 connectDB();
 
 app.use("/api/auth", userRoutes);
 app.use("/api/posts", blogRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
