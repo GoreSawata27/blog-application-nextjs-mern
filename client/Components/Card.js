b8e8df9836b5f821a5bc030609f32c1c18272127
Sawata gore <sawatagore27@gmail.com>
2024-09-14 18:04:24 +0530

    fixed card height issue due to long text

--- client/Components/Card.js
+++ client/Components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-export default function Card() {
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+export default function Card({ title, description, image, createdAt }) {
   return (
     <div className="card-container">
-      <img
-        src="https://img.freepik.com/free-vector/blogging-fun-content-creation-online-streaming-video-blog-young-girl-making-selfie-social-network-sharing-feedback-self-promotion-strategy-vector-isolated-concept-metaphor-illustration_335657-855.jpg"
-        alt="Company"
-      />
-      <div className="company-name">Company</div>
-      <div className="description">
-        Integer porta scelerisque sagittis, nunc mattis sit convallis nulla
-        ultricies fermentum et rhoncus egestas pretium tempor odio sed.
-      </div>
+      <img src={image} alt={title} />
+      <div className="company-name">{title}</div>
+      <div className="description">{description}</div>
       <div className="time">
-        <span className="icon">📅</span> 2 days ago
+        <span className="icon">📅</span> {formatDate(createdAt)}
       </div>
     </div>
   );
