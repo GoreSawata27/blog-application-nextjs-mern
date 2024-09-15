a1491ce100ad906713a5eb15fc0495117b2fb96c
Sawata gore <sawatagore27@gmail.com>
2024-09-15 17:48:23 +0530

    fixed avtar image issue

--- client/Components/dashboard/Navbar.js
+++ client/Components/dashboard/Navbar.js
@@ -17,12 +17,15 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div></div>
-      <div class="flex gap-6 -space-x-2 overflow-hidden">
-        <img
-          class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-          src="https://res.cloudinary.com/dgfc01mnb/image/upload/v1726373829/meteorologist-digital-avatar-_er1yxf.avif"
-          alt=""
-        />
+      <div className="flex gap-6 -space-x-2 overflow-hidden">
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          <img
+            class="inline-block h-10 w-10 rounded-full ring-2 ring-white useImage"
+            src="https://res.cloudinary.com/dgfc01mnb/image/upload/v1726373829/meteorologist-digital-avatar-_er1yxf.avif"
+            alt="useImage"
+          />
+        </div>
+
         <div className="flex flex-col ">
           <span className="username">{Name}</span>
           <span className="email">{Email}</span>
