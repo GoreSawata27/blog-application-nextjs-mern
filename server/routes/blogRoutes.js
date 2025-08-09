92ae342b192d408aa2a05f82a777120d72cd0d5c
Sawata Gore <sawatagore27@gmail.com>
2025-08-09 18:28:30 +0530

    updated routes

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { createBlog, getBlogs, getBlogById, updateBlog, deleteBlog } from "../controller/blogController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
 const router = express.Router();
 
-router.route("/").get(getBlogs).post(protect, createBlog);
-router.route("/:id").get(getBlogById).put(protect, updateBlog).delete(protect, deleteBlog);
+import { createBlog, getBlogs, getBlogById, updateBlog, deleteBlog } from "../controller/blogController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+
+// Public routes
+router.get("/", getBlogs);
+router.get("/:id", getBlogById);
+
+// Protected routes
+router.post("/", protect, createBlog);
+router.put("/:id", protect, updateBlog);
+router.delete("/:id", protect, deleteBlog);
 
 export default router;
