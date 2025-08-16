c83be735a43847f6ad7d818eb3f3ef26a4a4d782
Sawata Gore <sawatagore27@gmail.com>
2025-08-16 17:59:10 +0530

    added origin

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://deploy-frontend-tau.vercel.app", "http://localhost:3000"],
+    origin: ["https://deploy-frontend-tau.vercel.app", "http://localhost:3000", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
