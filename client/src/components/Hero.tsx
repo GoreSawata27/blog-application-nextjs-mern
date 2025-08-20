1f5f9906df112a593fd7ce95b64d60d695991daf
Gore Sawata <104631058+GoreSawata27@users.noreply.github.com>
2025-08-20 11:43:22 +0530

    Update Hero

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -3,7 +3,7 @@ type HeroProps = { name: string };
 export default function Hero({ name }: HeroProps) {
   return (
     <div>
-      <h1 className="text-2xl font-semibold">Welcome, {name} 👋</h1>
+      <h1 className="text-2xl font-semibold">Welcome 👋</h1>
       <p className="text-gray-500">Read and explore amazing blogs</p>
     </div>
   );
